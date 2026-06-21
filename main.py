08c4da0b0b6ff028330e38f163b0d146f651be41
Adhya <jhaadhya27@gmail.com>
2026-06-21 21:59:24 +0530

    Add expense reporting and total spendings calculations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("\033[35m-----PERSONAL EXPENSE TRACKER-----\033[0m")
 
-def addExpense():
+def add_Expense():
   amount=float(input("enter amount"))
   category=input("what did you spend this amount on? 🤔")
   date=input("enter date")
@@ -8,11 +8,36 @@ def addExpense():
   print(f"amount: {amount}")
   print(f"category: {category}")
   print(f"date: {date}")
+  with open("expense.csv","a") as file:
+    file.write(f"{date},{amount},{category}\n")
+
+def show_expenses():  #add category wise displaying expenses
+  print("<<<< Displaying your expenses >>>>") 
+  with open("expense.csv","r") as file:
+    for line in file:
+      print(line.strip())
+
+def total_spendings(): #add total spendings in one week,one month,one year, or custom
+  print("<<< Displaying your total expenses >>>")
+  total=0
+  with open("expense.csv","r") as file:
+    for line in file:
+      amt=float(line.split(",")[1])
+      total+=amt
+  
+  print(f"Total spending:{total}")
+      
+  
+
+  
+
 
 def show_menu():
   print("\n===MENU===")
   print("1.Add Expense")
-  print("2.Exit")
+  print("2.Show my Expenses")
+  print("3.Total Spendings")
+  print("4.Exit")
 
 def main():
   while True:
@@ -20,8 +45,12 @@ def main():
     ch=int(input("How do you want to proceed?"))
     if(ch==1):
       print("welcome to your personal expense analyzer!! adding expenses :)")
-      addExpense()
+      add_Expense()
     elif(ch==2):
+      show_expenses()
+    elif(ch==3):
+      total_spendings()
+    elif(ch==4):
       print("exiting the application....Goodbye!!")
       break
     else:
