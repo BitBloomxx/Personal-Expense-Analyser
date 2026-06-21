98045dc77c6c84e76f48146714963cf7f9f31191
Adhya <jhaadhya27@gmail.com>
2026-06-21 11:16:49 +0530

    menu+addExpense()

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("-----PERSONAL EXPENSE TRACKER-----")
+print("\033[35m-----PERSONAL EXPENSE TRACKER-----\033[0m")
+
+def addExpense():
+  amount=float(input("enter amount"))
+  category=input("what did you spend this amount on? 🤔")
+  date=input("enter date")
+  print("Expense Added-\n")
+  print(f"amount: {amount}")
+  print(f"category: {category}")
+  print(f"date: {date}")
+
+def show_menu():
+  print("\n===MENU===")
+  print("1.Add Expense")
+  print("2.Exit")
+
+def main():
+  while True:
+    show_menu()
+    ch=int(input("How do you want to proceed?"))
+    if(ch==1):
+      print("welcome to your personal expense analyzer!! adding expenses :)")
+      addExpense()
+    elif(ch==2):
+      print("exiting the application....Goodbye!!")
+      break
+    else:
+      print("Invalid Selection!! Please try again")
+      
+    
+main()
+
+
+    
+
+
